32b4f1e05b340128beee4f7b226663683b2904e2
Luiz Lima <110183537+luizgolima@users.noreply.github.com>
2023-01-30 15:42:50 -0300

    Add Project Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { Container } from "react-bootstrap";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Store from "./pages/Store";
+import About from "./pages/About";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Container>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Container>
   );
 }
 
---
+++ src/pages/Store.tsx
@@ -0,0 +1,9 @@
+function Store() {
+  return (
+    <div>
+      <h1>Store</h1>
+      <p>Welcome to the store page.</p>
+    </div>
+  );
+}
+export default Store;
