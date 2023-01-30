0ac69a2d646013005e707456b4307fd09760bd47
Luiz Lima <110183537+luizgolima@users.noreply.github.com>
2023-01-30 16:21:45 -0300

    Update Store.tsx

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,9 +1,18 @@
+import storeItems from "../data/items.json";
+import { Row, Col } from "react-bootstrap";
+
 function Store() {
   return (
-    <div>
+    <>
       <h1>Store</h1>
-      <p>Welcome to the store page.</p>
-    </div>
+      <Row xs={1} md={2} lg={3} className="g-3">
+        {storeItems.map((item) => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
   );
 }
 export default Store;
