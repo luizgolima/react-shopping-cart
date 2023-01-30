9b1c615ebb51b36700941efadfde2f2d47621880
Luiz Lima <110183537+luizgolima@users.noreply.github.com>
2023-01-30 16:30:49 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
+import { Routes, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
-import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Store from "./pages/Store";
-import About from "./pages/About";
-import Navbar from "./components/Navbar";
+import { Home } from "./pages/Home";
+import { Store } from "./pages/Store";
+import { About } from "./pages/About";
+import { Navbar } from "./components/Navbar";
+import { ShoppingCartProvider } from "./context/ShoppingCartContext";
 
 function App() {
   return (
-    <>
+    <ShoppingCartProvider>
       <Navbar />
       <Container className="mb-4">
         <Routes>
@@ -16,7 +17,7 @@ function App() {
           <Route path="/about" element={<About />} />
         </Routes>
       </Container>
-    </>
+    </ShoppingCartProvider>
   );
 }
 
